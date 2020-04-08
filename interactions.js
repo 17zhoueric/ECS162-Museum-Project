1a026f7a3cdbfc5e74e7517bcd3ee973a2df0df1
Eric Zhou <orion@Erics-MacBook-Air.local>
2020-04-08 03:46:55 -0700

    save before the fire

--- interactions.js
+++ interactions.js
@@ -1,40 +1,83 @@
+/***********************
+        GLOBALS
+************************/
+
+var pageIndex = document.getElementById("pageIndex");
+
+/***********************
+        COUNTER
+************************/
+//document.getElementsById("counter").textContent = "hello";
+
+function countdown() {
+    // calculate correct counting time
+    let finishTime = new Date("May 10, 2020 17:00:00").getTime();
+    let today = new Date().getTime();
+    let countingTime = (finishTime - today)/1000;
+    countingTime = Math.floor(countingTime);
+    // display
+    document.getElementsById("counter").textContent = countingTime.toLocaleString;
+}
+
+setInterval(countdown, 1000);
+
+/*************************
+        THUMBNAIL
+**************************/
+let button1Listener = document.getElementById("circleButton1");
+button1Listener.addEventListener("click", thumbnailChange1);
+
+let button2Listener = document.getElementById("circleButton2");
+button2Listener.addEventListener("click", thumbnailChange2);
+
+let button3Listener = document.getElementById("circleButton3");
+button3Listener.addEventListener("click", thumbnailChange3);
+
+let button4Listener = document.getElementById("circleButton4");
+button4Listener.addEventListener("click", thumbnailChange4);
+
+function thumbnailChange1() {
+    // change pageIndex
+    pageIndex.textContent = "1 / 4";
+    buttonChange();
+    artCaptionChange();
+    pictureChange();
+}
+
+function thumbnailChange2() {
+    // change pageIndex
+    pageIndex.textContent = "2 / 4";
+    buttonChange();
+    artCaptionChange();
+    pictureChange();
+}
+
+function thumbnailChange3() {
+    // change pageIndex
+    pageIndex.textContent = "3 / 4";
+    buttonChange();
+    artCaptionChange();
+    pictureChange();
+}
+
+function thumbnailChange4() {
+    // change pageIndex
+    pageIndex.textContent = "4 / 4";
+    buttonChange();
+    artCaptionChange();
+}
+
+/*************************
+        SLIDESHOW
+**************************/
+
 let leftArrowButtonListener = document.getElementById("leftArrow");
 leftArrowButtonListener.addEventListener("click", slideLeft);
 
-function slideLeft() {
-    // change pageIndex at top-left
-    var pageIndex = document.getElementById("pageIndex");
-    if (pageIndex.textContent[0] == "2") {
-        pageIndex.textContent = "1 / 4";
-    }
-    else if (pageIndex.textContent[0] == "3") {
-        pageIndex.textContent = "2 / 4";
-    }
-    else if (pageIndex.textContent[0] == "4") {
-        pageIndex.textContent = "3 / 4";
-    }
-    else {
-        pageIndex.textContent = "4 / 4";
-    }
-    // change artCaption at bottom
-    var artCaption = document.getElementById("artCaption");
-    if (pageIndex.textContent[0] == "2") {
-        artCaption.textContent = "Stephan Kaltenbach, Portrait of My Father, 1972-79. Acrylic on canvas, 114 x 170 3/4 inches.";
-        artCaption.style.textAlign = "center";
-    }
-    else if (pageIndex.textContent[0] == "3") {
-        artCaption.textContent = "Stephan Kaltenbach, Room Cube, 1967. Blueprint, 18 x 24 inches, edition of 10.";
-        artCaption.style.textAlign = "center";
-    }
-    else if (pageIndex.textContent[0] == "4") {
-        artCaption.textContent = "Stephan Kaltenbach, OPEN AFTER MY DEATH, 1970. Mild steel, engraved, with unknown contents, 3 x 6 x 3 inches.";
-        artCaption.style.textAlign = "center";
-    }
-    else {
-        artCaption.textContent = "Stephan Kaltenbach and guests, MODERN DRAPERY, 2020. Felt Fabric, 45 x 60 inches.";
-        artCaption.style.textAlign = "center";
-    }
-    // change opacity of button
+let rightArrowButtonListener = document.getElementById("rightArrow");
+rightArrowButtonListener.addEventListener("click", slideRight);
+
+function buttonChange() {
     var button1 = document.getElementById("circleButton1");
     var button2 = document.getElementById("circleButton2");
     var button3 = document.getElementById("circleButton3");
@@ -63,7 +106,59 @@ function slideLeft() {
         button3.style.opacity = 0.3;
         button4.style.opacity = 0.3; 
     }
-    // change picture
+}
+
+function artCaptionChange() {
+    var artCaption = document.getElementById("artCaption");
+    if (pageIndex.textContent[0] == "2") {
+        artCaption.textContent = "Stephan Kaltenbach, Portrait of My Father, 1972-79. Acrylic on canvas, 114 x 170 3/4 inches.";
+        artCaption.style.textAlign = "center";
+    }
+    else if (pageIndex.textContent[0] == "3") {
+        artCaption.textContent = "Stephan Kaltenbach, Room Cube, 1967. Blueprint, 18 x 24 inches, edition of 10.";
+        artCaption.style.textAlign = "center";
+    }
+    else if (pageIndex.textContent[0] == "4") {
+        artCaption.textContent = "Stephan Kaltenbach, OPEN AFTER MY DEATH, 1970. Mild steel, engraved, with unknown contents, 3 x 6 x 3 inches.";
+        artCaption.style.textAlign = "center";
+    }
+    else {
+        artCaption.textContent = "Stephan Kaltenbach and guests, MODERN DRAPERY, 2020. Felt Fabric, 45 x 60 inches.";
+        artCaption.style.textAlign = "center";
+    }
+}
+
+function pageIndexChangeLeft() {
+    if (pageIndex.textContent[0] == "2") {
+        pageIndex.textContent = "1 / 4";
+    }
+    else if (pageIndex.textContent[0] == "3") {
+        pageIndex.textContent = "2 / 4";
+    }
+    else if (pageIndex.textContent[0] == "4") {
+        pageIndex.textContent = "3 / 4";
+    }
+    else {
+        pageIndex.textContent = "4 / 4";
+    }
+}
+
+function pageIndexChangeRight() {
+    if (pageIndex.textContent[0] == "2") {
+        pageIndex.textContent = "3 / 4";
+    }
+    else if (pageIndex.textContent[0] == "3") {
+        pageIndex.textContent = "4 / 4";
+    }
+    else if (pageIndex.textContent[0] == "4") {
+        pageIndex.textContent = "1 / 4";
+    }
+    else {
+        pageIndex.textContent = "2 / 4";
+    }
+}
+
+function pictureChange() {
     var fabric = document.getElementsByClassName("fabric");
     var fatherPortrait = document.getElementsByClassName("fatherPortrait");
     var roomCube = document.getElementsByClassName("roomCube");
@@ -92,4 +187,26 @@ function slideLeft() {
         roomCube.style.display = "none";
         openAfterDeath.style.display = "none";
     }
+}
+
+function slideLeft() {
+    // change pageIndex at top-left
+    pageIndexChangeLeft();
+    // change artCaption at bottom
+    artCaptionChange();
+    // change opacity of button
+    buttonChange();
+    // change picture
+    pictureChange();
+}
+
+function slideRight() {
+    // change pageIndex at top-left
+    pageIndexChangeRight();
+    // change artCaption at bottom
+    artCaptionChange();
+    // change opacity of button
+    buttonChange();
+    // change picture
+    pictureChange();
 }
